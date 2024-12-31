9a75f98c33a157faa82563c008923c5f690e8a77
MesVortex <meskinemostafa4@gmail.com>
2024-12-31 16:46:16 +0100

    MS-9 Configure modular architecture & lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'library', loadChildren: () => import('./features/library/library.module').then(m => m.LibraryModule) },
+  { path: 'track', loadChildren: () => import('./features/track/track.module').then(m => m.TrackModule) },
+  { path: '', redirectTo: 'library', pathMatch: 'full' },
+  { path: '**', redirectTo: 'library' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
